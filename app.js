8e7523d7d58c95b234eb30b90aa32f94deb2f06e
Julian Barrio <julian.barrio@hotmail.com>
2022-08-01 22:53:43 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+ let alumno = prompt("Ingresa el nombre del alumno o LISTO para terminar")
+ 
+ while(alumno != "LISTO"){
+    let presentismo = prompt("El alumno " + alumno + " esta presente si o no")
+     console.log("El alumno " + alumno + " " + presentismo + " esta presente")
+     alumno = prompt("Ingresa el nombre del alumno o LISTO para terminar")
+     
+ }
